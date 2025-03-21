4b3c2625f820561b45378421f9ade8562b90d727
Ruben Bohorquez <r.bohorquezg3177@gmail.com>
2025-03-21 01:56:54 -0300

    More prints

--- netcat_test.sh
+++ netcat_test.sh
@@ -3,11 +3,14 @@
 echo "Installing netcat..."
 apk add --no-cache --quiet netcat-openbsd > /dev/null 2>&1
 
-echo "Sending test message..."
-RESPONSE=$(echo "Test message" | nc -v server.tp0_testing_net 12345)
+MESSAGE="lorem ipsum dolor sit amet"
+
+echo "Sending test message: $MESSAGE"
+RESPONSE=$(echo "$MESSAGE" | nc -v server.tp0_testing_net 12345)
+echo "Received response: $RESPONSE"
 
 # Check if response matches sent message
-if [ "$RESPONSE" = "Test message" ]; then
+if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "Test successful"
     exit 0
 else
