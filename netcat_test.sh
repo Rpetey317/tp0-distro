a17779a7ec80c6ef362e9bf5757558af9ba3265f
Ruben Bohorquez <r.bohorquezg3177@gmail.com>
2025-03-23 21:00:44 -0300

    Found the culprit

--- netcat_test.sh
+++ netcat_test.sh
@@ -6,7 +6,7 @@ apk add --no-cache --quiet netcat-openbsd > /dev/null 2>&1
 MESSAGE="lorem ipsum dolor sit amet"
 
 echo "Sending test message: $MESSAGE"
-RESPONSE=$(echo "$MESSAGE" | nc -v server.tp0_testing_net 12345)
+RESPONSE=$(echo "$MESSAGE" | nc -q 0 -v server.tp0_testing_net 12345)
 echo "Received response: $RESPONSE"
 
 # Check if response matches sent message
