810910685877067977226149d507a8f21c287475
Ruben Bohorquez <r.bohorquezg3177@gmail.com>
2025-03-26 11:55:56 -0300

    implemented lottery draw on server

--- server/common/server.py
+++ server/common/server.py
@@ -3,11 +3,11 @@ import logging
 import signal
 import threading
 from .protocol import ServerProtocol
-from .utils import store_bets
+from .utils import store_bets, load_bets, has_won
 import traceback
 
 class Server:
-    def __init__(self, port, listen_backlog):
+    def __init__(self, port, listen_backlog, n_agencies):
         # Initialize server socket
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.bind(('', port))
@@ -16,7 +16,7 @@ class Server:
         self._protocol = None
         self._mutex = threading.Lock()
         self._running = True
-        
+        self._n_agencies = n_agencies
     def run(self):
         """
         Dummy Server loop
@@ -39,7 +39,8 @@ class Server:
             
         signal.signal(signal.SIGTERM, handle_sigterm)
         
-        while self._running:
+        processed_agencies = 0
+        while self._running and processed_agencies < self._n_agencies:
             try:
                 bets = []
                 client_sock, _ = self._socket.accept()
@@ -54,8 +55,20 @@ class Server:
                 logging.error(f'action: recv_messages | result: fail | error: {e}')
                 logging.error(traceback.format_exc())
                 continue
-            
+            finally:
+                processed_agencies += 1
+                
+        self.draw_lottery()
         logging.info('action: shutdown | result: success')
+
+    def draw_lottery(self):
+        logging.info('action: sorteo | result: in_progress')
+        with self._mutex:
+            bets = load_bets()
+            for bet in bets:
+                if has_won(bet):
+                    logging.info(f'action: ganadores | result: success | ganador: {bet.first_name} {bet.last_name} | numero: {bet.number}')
+        logging.info('action: sorteo | result: success')
 
     def shutdown(self):
         logging.info('action: shutdown | result: in_progress')
